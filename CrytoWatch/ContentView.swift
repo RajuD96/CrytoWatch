adcf25278a589865ccd1124b3c2c06717ee75f7c
RajuD96 <rajudhumne@gmail.com>
2024-01-26 23:20:06 +0000

    Initial Commit

---
+++ CrytoWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrytoWatch
+//
+//  Created by Raju Dhumne on 26/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrytoWatch/CrytoWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrytoWatchApp.swift
+//  CrytoWatch
+//
+//  Created by Raju Dhumne on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CrytoWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
