c081678cd3ff5ac6581fc93d551ab4cce62c51a5
rajudhumne <rajudhumne@gmail.com>
2024-02-01 19:22:14 +0000

    Intr

--- CrytoWatch/Core/Components/CoinImage/CoinImageViewModel.swift
+++ CrytoWatch/Core/Components/CoinImage/CoinImageViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageViewModel: ObservableObject{
+    @Published var image: UIImage?
+    @Published var isLoading: Bool = false
+    
+    private let coin: CoinModel
+    private let dataService: CoinImageService
+    private var cancellable = Set<AnyCancellable>()
+    
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.dataService = CoinImageService(coin: coin)
+        self.addSubscriber()
+    }
+    
+    private func addSubscriber() {
+        dataService.$image
+            .sink { [weak self] (_) in
+                self?.isLoading = true
+            } receiveValue: { [weak self] returnedImage in
+                self?.isLoading = false
+                self?.image = returnedImage
+            }
+            .store(in: &cancellable)
+
+    }
+}
--- CrytoWatch/Services/CoinImageService.swift
+++ CrytoWatch/Services/CoinImageService.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage?
+    private var imageSubscriptionCancellable: AnyCancellable?
+    
+    private let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getImage()
+    }
+    
+    private func getImage() {
+        guard let url = URL(string: coin.image)
+        else { return }
+        
+        imageSubscriptionCancellable = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: {[weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscriptionCancellable?.cancel()
+            })
+    }
+}
