b0bc2d78d71af085c5accf05e9428ed4cd78a3f2
rajudhumne <rajudhumne@gmail.com>
2024-02-01 18:34:06 +0000

    added CoinImageView File

---
+++ CrytoWatch/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinImageView.swift
+//  CrytoWatch
+//
+//  Created by Raju Dhumne on 01/02/24.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinImageView()
+}
